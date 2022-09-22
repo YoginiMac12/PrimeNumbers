7b7bda1f61b8b0cdb6c0a93ea491498a86d48b2b
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-22 23:13:32 +0530

    Add files via upload

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+         int count = 0;
+         Console.WriteLine("1 to 100 Prime Numbers:");
+         for (int i = 2; i <= 100; i++)
+         {
+           for (int j = 2; j <= 100; j++)
+           {
+             if(i%j==0)
+             count++;
+           }
+           if (count == 1)
+           {
+           Console.WriteLine(i);
+           }
+           count = 0;
+         }
+            Console.ReadLine();
+        }
+    }
+}
+
